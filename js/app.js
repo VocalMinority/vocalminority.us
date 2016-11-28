68ebb7f2457022d96b463bb299ef7e911802f1b7
Kevin Ball <kmball11@gmail.com>
2016-11-27 21:42:27 -0800

    Remove now-unnecessary js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,1 @@
 $(document).foundation();
-
-$('#mc-embedded-subscribe-form').on('submit', function() {
-  $('[data-equalizer]').trigger('resizeme.zf.trigger')
-});
-
-// Wrap mailchimp callback to force resize
-window.mc.ajaxOptions = {
-  url: mc.getAjaxSubmitUrl(),
-  type: 'GET',
-  dataType: 'json',
-  contentType: "application/json; charset=utf-8",
-  success: function(resp) {
-    mc.mce_success_cb(resp);
-    $('[data-equalizer]').trigger('resizeme.zf.trigger')
-  }
-};
