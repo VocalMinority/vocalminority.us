7df20184936bd3e91088d0ed2ae9a0784c420b4c
Kevin Ball <kmball11@gmail.com>
2016-11-28 21:04:39 -0800

    Add new success state on email

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation();
+
+
+if(window.mc) {
+  // Wrap mailchimp callback
+  window.mc.ajaxOptions = {
+    url: mc.getAjaxSubmitUrl(),
+    type: 'GET',
+    dataType: 'json',
+    contentType: "application/json; charset=utf-8",
+    success: function(resp) {
+      resp.msg = "Thanks for joining! Check your email to confirm your membership.";
+      mc.mce_success_cb(resp);
+      $('[data-hide-on-success]').hide();
+    }
+  };
+}
