44bcfed5192bdf02b87301796d31e427461e514a
Christopher Chan <chris@able.co>
2016-11-29 19:09:36 -0800

    Post-sign-up tweaks
    - Hide instruction paragraph
    - Include copy to email us if you don't receive confirmation

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ if(window.mc) {
     dataType: 'json',
     contentType: "application/json; charset=utf-8",
     success: function(resp) {
-      resp.msg = "Thanks for joining! Check your email to confirm your membership.";
+      resp.msg = "Thanks for joining! Check your inbox to confirm your membership. If you don't get one in the next few minutes, email hello@vocalminority.us and we'll get you on the list.";
       mc.mce_success_cb(resp);
       $('[data-hide-on-success]').hide();
     }
