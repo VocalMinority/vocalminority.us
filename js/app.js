46cb6a6b76d94694bd0ad2c2748ebf7fbd817af5
Kevin Ball <kmball11@gmail.com>
2016-12-12 20:57:21 -0800

    Set up basic post-join flow to donatin

--- js/app.js
+++ js/app.js
@@ -9,8 +9,11 @@ if(window.mc) {
     dataType: 'json',
     contentType: "application/json; charset=utf-8",
     success: function(resp) {
-      var amount = $('input[name="AMOUNT"]').val();
-      window.location = '/donate.html?amount=' + amount;
+      var amount = $('input[name="AMOUNT"]:checked').val();
+      $('#amount-to-donate').text('$' + amount);
+      $('#mc-embedded-subscribe-form').addClass('hide');
+      $('#confirmation').removeClass('hide').hide().fadeIn();
+      window.scrollTo(0, $('#confirmation').offset().top - 30);
     }
   };
 }
