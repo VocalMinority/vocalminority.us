8109999dcff60072a36e05cf8b770bc01dfea4a0
Noah Silas <noahking@gmail.com>
2017-01-14 23:46:03 +0900

    HACK: Fix FAQ toggles
    The Mailchimp validation code seems to be run even on pages that don't
    include the Mailchimp form, causing a JS error to prevent any later code
    from executing.
    
    I suspect that this is related to the JS compilation step optimizing out
    a conditional, but I haven't investigated thoroughly yet.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ ga('send', 'pageview');
 
 $(document).foundation();
 
+// Setup FAQ item toggling
+$('.toggle').on('click', function() { $(this).toggleClass('closed'); });
 
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -98,6 +100,3 @@ $("#mc-embedded-subscribe-form").ajaxForm({
     window.scrollTo(0, $('#confirmation').offset().top - 30);
   }
 });
-
-// Setup FAQ item toggling
-$('.toggle').on('click', function() { $(this).toggleClass('closed'); });
